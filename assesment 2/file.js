fc5023064226cea575f8ffe05b0d75fd3e0f1f18
Gowtham <gowthamsumith@gmail.com>
2019-03-30 00:02:36 +0530

    Changes on Assesment 2
    use youtube video in my page

--- assesment 2/file.js
+++ assesment 2/file.js
@@ -1,8 +1,9 @@
-console.log(cityList);
-var cityListJSONKey = Object.keys(cityList);
-console.log(cityListJSONKey.length);
-var elementString = "";
-for(var i = 0; i <= 59; i++){
+var cityListJSONKey = Object.keys(cityList),
+    elementString = "",
+    cityListLength = cityListJSONKey.length;
+
+// Gererate element for movie list card
+for(var i = 0; i <= cityListLength - 1; i++){
     var posterImage = cityList[cityListJSONKey[i]].EventCode != undefined ? "https://in.bmscdn.com/events/moviecard/"+cityList[cityListJSONKey[i]].EventCode+".jpg" : "";
     elementString += '<div class="movie-container" id="'+cityList[cityListJSONKey[i]].EventCode+'">'
                         +'<img src="'+posterImage+'" class="movie-poster"/>'
@@ -16,17 +17,70 @@ for(var i = 0; i <= 59; i++){
             elementString += '<span>'+categoryArr[j]+'</span>';
         }
     }
-    elementString +=        '<button class="viewTrailer">View Trailer</button>'
+    elementString +=        '<button class="viewTrailer" data-id="'+cityList[cityListJSONKey[i]].EventCode+'">View Trailer</button>'
+                        +'</div>'
+                        +'<div class="card-footer">'
+                            +'<h5>'+cityList[cityListJSONKey[i]].EventTitle+'</h5>'
                         +'</div>'
                     +'</div>';
 }
 document.getElementsByClassName('trailer-container')[0].innerHTML = elementString;
 
-for(var i = 0; i <= 59; i++){
+// Add event for cancel button which is in the trailer container
+var calcelEvent = document.getElementById('cancel');
+function removeTrailer(){
+    document.getElementById('videoContainer').remove();
+}
+var viewTrailer = document.getElementsByClassName('viewTrailer');
+for (var i = 0; i < viewTrailer.length; i++) {
+    viewTrailer[i].addEventListener('click', showTrailer);
+}
+for(var i = 0; i <= cityListLength - 1; i++){
     var myObject = document.getElementsByClassName('hidden-div')[i];
     myObject.addEventListener("mouseenter", setOpacity);
     myObject.addEventListener("mouseleave", removeOpacity);
-   
+}
+function showTrailer(event){
+    var trailerId = event.target.getAttribute("data-id"),
+        trailerButtonCoordinate = event.target.getBoundingClientRect(),
+        rowFirstElementId = document.elementFromPoint(68, trailerButtonCoordinate.y).parentElement.getAttribute('id'),
+        trailerDetails = cityList[trailerId];
+        if(document.getElementById('videoContainer') != null){
+            document.getElementById('videoContainer').remove();
+        }
+    var list = document.getElementsByClassName("trailer-container")[0],
+        el = document.getElementById(rowFirstElementId),
+        nodes = Array.prototype.slice.call(list.children),
+        elIndex = nodes.indexOf(el),
+        elementDiv = document.createElement('div'),
+        trailerURL = trailerDetails.TrailerURL.substr(trailerDetails.TrailerURL.indexOf("v=")+2, trailerDetails.TrailerURL.length);
+        console.log(trailerButtonCoordinate);
+    if(trailerURL.indexOf("&") > -1){
+        trailerURL = trailerURL.substr(0, trailerURL.indexOf("&"));
+    }
+    elementDiv.id = 'videoContainer';
+    var e = document.createElement('div');
+    e.id = 'video-placeholder'
+    elementDiv.appendChild(e);
+    var spanEle = document.createElement('span');
+    spanEle.innerHTML = "x";
+    spanEle.id = 'cancel';
+    spanEle.onclick = removeTrailer;
+    elementDiv.appendChild(spanEle);
+    list.insertBefore(elementDiv, list.childNodes[elIndex]);
+    
+    // For use youtube video in my page
+    player = new YT.Player('video-placeholder', {
+        width: 790,
+        height: 500,
+        videoId: trailerURL,
+        playerVars: {
+            color: 'white',
+        },
+        events: {
+        }
+    });
+    document.getElementById('videoContainer').scrollIntoView(true);
 }
 function setOpacity(event){
     event.target.previousElementSibling.style.opacity = 0.3;
--- assesment 2/index.php
+++ assesment 2/index.php
@@ -6,21 +6,29 @@
 
     curl_close($ch);
 ?>
+<head>
+    <title>Movie's Trailer</title>
+</head>
 <style>
     body{
         background-color: #000;
     }
     .movie-container{
-        /* grid-area: movie; */
+        /* grid-area: movie-container; */
+        /* margin: auto;
+        height: 300px; */
         margin: auto;
         height: 300px;
+        width: 200px;
+        display: inline-flex;
+        margin:42px;
     }
-    .trailer-container{
+    /* .trailer-container{
         display: grid;
         grid-column-gap: 30px;
         grid-row-gap: 30px;
         grid-template-rows: 1fr 1fr 1fr 1fr;
-        grid-template-areas: "movie movie movie movie";
+        grid-template-areas: "movie-container movie-container movie-container movie-container";
         padding: 10px;
         padding: 38px 60px;
     }
@@ -30,7 +38,7 @@
             grid-column-gap: 30px;
             grid-row-gap: 30px;
             grid-template-columns: 1fr 1fr 1fr;
-            grid-template-areas: "movie movie movie";
+            grid-template-areas: "movie-container movie-container movie-container";
             padding: 10px;
         }
     }
@@ -40,10 +48,10 @@
             grid-column-gap: 30px;
             grid-row-gap: 30px;
             grid-template-columns: 1fr 1fr;
-            grid-template-areas: "movie movie";
+            grid-template-areas: "movie-container movie-container";
             padding: 10px;
         }
-    }
+    } */
     .movie-poster{
         width: 190px;
         height: 280px;
@@ -51,7 +59,7 @@
         border-radius: 4px;
     }   
     .hidden-div{
-        width: 202px;
+        /* width: 202px;
         height: 292px;
         position: relative;
         top: -292px;
@@ -60,7 +68,16 @@
         text-align: center;
         font-size: 26px;
         font-weight: bold;
+        color: #FFF; */
+        width: 199px;
+        height: 289px;
+        position: absolute;
+        opacity: 0;
+        text-align: center;
+        font-size: 18px;
+        font-weight: bold;
         color: #FFF;
+        text-transform: uppercase;
     } 
     .hidden-div span{
         position: relative;
@@ -75,6 +92,7 @@
     .hidden-div:hover + .movie-poster{
         opacity: 0.5;
     }
+
     .viewTrailer{
         background: #0078ff;
         padding: 5px 10px;
@@ -91,11 +109,53 @@
         border: none;
         transform: translateX(-50%);
     }
+    #video-placeholder{
+        width:60%;
+    }
+    #videoContainer{
+        width: 94%;
+        border: 6px solid #FFF;
+        margin: auto;
+        padding: 22px;
+        border-radius: 4px;
+        margin-top: 25px;
+        margin-bottom: 25px;
+    }
+    #cancel{
+        position: relative;
+        top: -489px;
+        right: -520px;
+        color: #FFF;
+        font-size: 50px;
+        font-family: fantasy;
+        cursor: pointer;
+    }
+    .header-title{
+        color: #FFF;
+        text-align: center;
+        font-family: cursive;
+
+    }
+    .card-footer{
+        width: 410px;
+        position: relative;
+        color: #FFF;
+        top: 290px;
+        left: -200px;
+    }
+    .card-footer h5{
+        margin: 0;
+        padding: 10px 0;
+        width: 200px;
+        font-family: sans-serif;
+    }
 </style>
+<h2 class="header-title">Movie's Trailer</h2>
 <div class="trailer-container">
 </div>
 <script>
     var cityList = <?php echo $output ?>;
     cityList = cityList[1];
 </script> 
+<script type="text/javascript" src="https://www.youtube.com/iframe_api"></script>
 <script type="text/javascript" src="file.js"></script>
