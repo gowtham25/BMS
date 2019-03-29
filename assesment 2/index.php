582b7661d57eb7787d05cf09bbe09d6b4c676583
Gowtham <gowthamsumith@gmail.com>
2019-03-30 00:07:02 +0530

    Update assesment 2/index.php
    Remove unnecessary code

--- assesment 2/index.php
+++ assesment 2/index.php
@@ -14,44 +14,12 @@
         background-color: #000;
     }
     .movie-container{
-        /* grid-area: movie-container; */
-        /* margin: auto;
-        height: 300px; */
         margin: auto;
         height: 300px;
         width: 200px;
         display: inline-flex;
         margin:42px;
     }
-    /* .trailer-container{
-        display: grid;
-        grid-column-gap: 30px;
-        grid-row-gap: 30px;
-        grid-template-rows: 1fr 1fr 1fr 1fr;
-        grid-template-areas: "movie-container movie-container movie-container movie-container";
-        padding: 10px;
-        padding: 38px 60px;
-    }
-    @media (max-width: 1000px) and (min-width:670px){
-        .trailer-container{
-            display: grid;
-            grid-column-gap: 30px;
-            grid-row-gap: 30px;
-            grid-template-columns: 1fr 1fr 1fr;
-            grid-template-areas: "movie-container movie-container movie-container";
-            padding: 10px;
-        }
-    }
-    @media (max-width: 670px){
-        .trailer-container{
-            display: grid;
-            grid-column-gap: 30px;
-            grid-row-gap: 30px;
-            grid-template-columns: 1fr 1fr;
-            grid-template-areas: "movie-container movie-container";
-            padding: 10px;
-        }
-    } */
     .movie-poster{
         width: 190px;
         height: 280px;
@@ -59,16 +27,6 @@
         border-radius: 4px;
     }   
     .hidden-div{
-        /* width: 202px;
-        height: 292px;
-        position: relative;
-        top: -292px;
-        left: 0px;
-        opacity: 0;
-        text-align: center;
-        font-size: 26px;
-        font-weight: bold;
-        color: #FFF; */
         width: 199px;
         height: 289px;
         position: absolute;
